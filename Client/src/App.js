7cdbf62a4f5856d0b11d0b5347935dc83a7abb07
idoasisay <59815596+idoasisay@users.noreply.github.com>
2021-02-24 19:31:18 +0900

    직선 만들기까지 성공

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,6 +1,6 @@
 import { React, createRef, useEffect, useState } from 'react';
 import './App.css';
-
+//https://jsfiddle.net/richardcwc/cvem3wuv/
 // // setting CSS variables inside this JS where they
 // // can be declaratively used inside the CSS file.
 // document.addEventListener('mousemove', function(e) {
@@ -13,45 +13,74 @@ function App() {
   let canvas;
   let canvasRef = createRef(); // Ref를 사용해 캔버스의 Dom 값을 가지고 옴
 
-  let pos = {
-    drawable: false,
-    x: -1,
-    y: -1,
-  };
+  let drawable = false;
+  let cursorX,
+    cursorY = -1;
+  let firstClick = [0, 0];
+
   let ctx; //컨택스트
 
-  let [a, aset] = useState([initDraw, draw, finishDraw, finishDraw]);
   useEffect(() => {
     canvas = canvasRef.current;
     ctx = canvas.getContext('2d');
-    canvas.addEventListener('mousedown', a[0]);
-    canvas.addEventListener('mousemove', a[1]);
-    canvas.addEventListener('mouseup', a[2]);
-    canvas.addEventListener('mouseout', a[3]);
+
+    canvas.addEventListener('mousedown', initDraw);
+    canvas.addEventListener('mousemove', draw);
+    canvas.addEventListener('mouseup', finishDraw);
+    canvas.addEventListener('mouseout', finishDraw);
+    canvas.addEventListener('click', clickDraw, false);
   }, []); // []를 사용하면 디드마운트 사용하는 것과 같다
-
-  function getPosition(event) {
-    return { X: event.offsetX, Y: event.offsetY };
-  }
-
-  function initDraw(event) {
-    ctx.beginPath();
-    pos = { drawable: true, ...getPosition(event) };
-    ctx.moveTo(pos.X, pos.Y);
-    console.log(pos);
-  }
 
   //손을 떼면 -1이 되고, 클릭하면 거기에 잡히니까... 여기에다가 라인을 주면 된다.
   //클릭할 때 직선이 되어야 함
-  function draw(event) {
-    if (pos.drawable) {
-      pos = { ...pos, ...getPosition(event) }; // 얘가 안 되는 것 같은데...
-      ctx.lineTo(pos.X, pos.Y);
+  let isClick = false;
+
+  function clickDraw(e) {
+    console.log(drawable);
+    if (!isClick) {
+      isClick = true;
+      cursorX = e.pageX;
+      cursorY = e.pageY;
+    } else if (isClick) {
+      console.log(1);
+      ctx.moveTo(firstClick[0], firstClick[1]);
+      ctx.lineTo(cursorX, cursorY);
       ctx.stroke();
+      ctx.closePath();
+      isClick = false;
     }
+  }
+  function initDraw(e) {
+    // firstClick = [e.pageX, e.pageY];
+    // ctx.beginPath();
+    // ctx.moveTo(firstClick[0], firstClick[1]);
+    // ctx.lineTo(cursorX, cursorY);
+    //ctx.lineTo(e.offsetX, e.offsetY);
+    //ctx.stroke();
+  }
+
+  function draw(e) {
+    drawable = true;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    firstClick = [e.pageX, e.pageY];
+    ctx.beginPath();
+    ctx.moveTo(firstClick[0], firstClick[1]);
+    ctx.lineTo(cursorX, cursorY);
+
+    //ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    //console.log(firstClick);
+    // if (drawable) {
+    //   //pos = { ...pos, ex: event.offsetX, ey: event.offsetY }; // 얘가 안 되는 것 같은데...
+    //   ctx.moveTo(e.offsetX, e.offsetY);
+    //   ctx.stroke();
+    //   ctx.clearRect(0, 0, canvas.width, canvas.height);
+    // }
   }
 
   function finishDraw() {
+    ctx.stroke();
+    ctx.closePath();
     //pos = { drawable: false, X: -1, Y: -1 };
   }
 
